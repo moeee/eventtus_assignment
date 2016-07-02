a170b0086071085ccaa98bb7bb897a0d439921a3
Mohamed Attia <mohamed.attia@egabi.com>
2016-07-02 13:28:37 +0200

    initial commit

---
+++ app/src/main/java/com/eventtus/twitterapp/Globals.java
@@ -0,0 +1,16 @@
+package com.eventtus.twitterapp;
+
+import android.content.SharedPreferences;
+
+import com.twitter.sdk.android.core.TwitterSession;
+
+
+public class Globals {
+
+    public static final String TWITTER_KEY = "2h7w3kj7xOHKTrTCGWU70wkgu";
+    public static final String TWITTER_SECRET = "UJsfczo3vELkxkRazfKjIColBy2CRuHz8aw6zuzNHB9IwFj3jz";
+    public static TwitterSession session;
+    public static String userID="";
+    public static String userName="";
+    public static SharedPreferences prefs;
+}
--- app/src/main/java/com/eventtus/twitterapp/MainActivity.java
+++ app/src/main/java/com/eventtus/twitterapp/MainActivity.java
@@ -1,13 +1,75 @@
 package com.eventtus.twitterapp;
 
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.os.StrictMode;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
+
+import com.twitter.sdk.android.Twitter;
+import com.twitter.sdk.android.core.Callback;
+import com.twitter.sdk.android.core.Result;
+import com.twitter.sdk.android.core.TwitterAuthConfig;
+import com.twitter.sdk.android.core.TwitterException;
+import com.twitter.sdk.android.core.TwitterSession;
+import com.twitter.sdk.android.core.identity.TwitterLoginButton;
+
+import io.fabric.sdk.android.Fabric;
 
 public class MainActivity extends AppCompatActivity {
+
+    TwitterLoginButton loginButton;
+    ProgressDialog pd;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        TwitterAuthConfig authConfig = new TwitterAuthConfig(Globals.TWITTER_KEY, Globals.TWITTER_SECRET);
+        Fabric.with(this, new Twitter(authConfig));
         setContentView(R.layout.activity_main);
+
+        // give permission to StrictMode
+        StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+        StrictMode.setThreadPolicy(policy);
+
+        getViews();
+
+        loginButton.setCallback(new Callback<TwitterSession>() {
+            @Override
+            public void success(Result<TwitterSession> result) {
+
+                Globals.session = result.data;
+
+                long userid = Globals.session.getUserId();
+                Globals.userID = Long.toString(userid);
+                Globals.userName = Globals.session.getUserName();
+
+                System.out.println("attia successfully logged in .. hi "+Globals.session.getUserName());
+
+            }
+
+            @Override
+            public void failure(TwitterException exception) {
+                Log.d("Error", "Login with Twitter failure", exception);
+                Toast.makeText(getApplicationContext(), "Connection Error !!",Toast.LENGTH_LONG).show();
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        loginButton.onActivityResult(requestCode, resultCode, data);
+    }
+
+    void getViews(){
+        String pdw = "Loading.. Please wait";
+        pd = new ProgressDialog(this);
+        pd.setCancelable(false);
+        pd.setMessage(pdw);
+
+        loginButton = (TwitterLoginButton) findViewById(R.id.twitter_login_button);
     }
 }
